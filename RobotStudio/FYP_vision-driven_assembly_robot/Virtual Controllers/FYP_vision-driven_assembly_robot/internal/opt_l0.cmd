b40dca6c5e7d3f33c6b8d6f7ead139807a85f0a5
Antonio Pozo <pozoleonantonio@gmail.com>
2023-08-11 10:27:47 +0200

    v00

---
+++ RobotStudio/FYP_vision-driven_assembly_robot/Virtual Controllers/FYP_vision-driven_assembly_robot/internal/opt_l0.cmd
@@ -0,0 +1,2 @@
+
+include -path "C:/Users/anton/AppData/Local/ABB/RobotWare/RobotControl_7.8.2/options/sc/ScInc_opt.cmd"
---
+++ RobotStudio/FYP_vision-driven_assembly_robot/Virtual Controllers/FYP_vision-driven_assembly_robot/internal/option.cmd
@@ -0,0 +1,2 @@
+
+include -path "C:/Users/anton/AppData/Local/ABB/RobotWare/RobotControl_7.8.2/options/sc/scopt.cmd"
